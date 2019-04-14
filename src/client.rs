f6bab557b96d4f240550d9a1d8ab552134f444c5
Rob Saunders <rob@wearebrandnew.com>
2019-04-15 05:21:29 +0800

    closer to signed authentication

--- src/client.rs
+++ src/client.rs
@@ -5,6 +5,7 @@ use reqwest::{Response, StatusCode};
 use ring::{digest, hmac};
 //use base64;
 //use time;
+use core::fmt::Debug;
 use std::collections::BTreeMap;
 
 mod account;
@@ -43,7 +44,6 @@ impl Client {
     }
 
     pub fn get_signed(&self, endpoint: &str, request: &str) -> APIResult<String> {
-        //        let sig = calculate_signature(endpoint, request);
         let url = format!("{}{}{}", API_HOST, endpoint, request);
         let client = reqwest::ClientBuilder::new().build()?;
 
@@ -51,12 +51,16 @@ impl Client {
         params.insert("AccessKeyId".to_string(), self.api_key.clone());
         params.insert("SignatureMethod".to_string(), "HmacSHA256".to_string());
         params.insert("SignatureVersion".to_string(), "2".to_string());
-        params.insert(
-            "Timestamp".to_string(),
-            "2017-05-
-11T15%3A19%3A30"
-                .to_string(),
-        );
+
+        // time
+        use percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
+        use std::time::{SystemTime, UNIX_EPOCH};
+
+        let utc_time = chrono::Utc::now();
+        let formatted_time = utc_time.format("%Y-%m-%dT%H%%3A%M%%3A%S").to_string();
+        println!("timestamp: {}", formatted_time.clone());
+        params.insert("Timestamp".to_string(), formatted_time);
+        // end time
 
         let query_string = build_query_string(params);
         let signature = sign_hmac_sha256_base64(&self.secret_key, &query_string);
@@ -220,11 +224,38 @@ pub fn build_query_string(mut parameters: BTreeMap<String, String>) -> String {
 /// Compiles query string parameters into a http query string, in byte-order.
 ///
 /// ```rust
-/// assert_eq!(huobi::client::sign_hmac_sha256_base64("b0xxxxxx-c6xxxxxx-94xxxxxx-dxxxx", "GET\napi.huobi.pro\n/v1/order/orders\nAccessKeyId=e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890"), "4F65x5A2bLyMWVQj3Aqp+B4w+ivaA7n5Oi2SuYtCJ9o=");
+/// assert_eq!(huobi::client::sign_hmac_sha256_base64("b0xxxxxx-c6xxxxxx-94xxxxxx-dxxxx", "GET\napi.huobi.pro\n/v1/order/orders\nAccessKeyId=e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890"), "Nmd8AU8uAe0mkFpxNbiava0aeZzBEtYjCdie1ZYZjoM=");
 /// ```
 pub fn sign_hmac_sha256_base64(secret: &str, params: &str) -> String {
+    //    let signed_key = hmac::SigningKey::new(&digest::SHA256, secret.as_bytes());
+    //    //    hmac::sign(&signed_key, params.as_bytes());
+    //    //    let signature = hex_encode(hmac::sign(&signed_key, params.as_bytes()));
+    //    let signature = hmac::sign(&signed_key, params.as_bytes());
+    //    //    println!("  - {:02x?}", signature);
+    //    //    let signature = format!("{:02x?}", signature);
+    //    let base64_url = base64::encode(&signature.as_bytes());
+    //    hex_encode(base64_url)
+
+    //    use crypto::digest::Digest;
+    //    use crypto::sha2::Sha256;
+    //    use serialize::base64::{ToBase64, STANDARD};
+    //
+    //    let mut sha = Sha256::new();
+    //    sha.input_str(secret);
+    //    //    Vec::from_elem(sha.output_bytes(), 0u8);
+    //    Vec::from_iter(repeat(0u8).take(sha.output_bytes()));
+    //
+    //    sha.result(params.as_mut_slice());
+    //    format!("{}", bytes.to_base64(STANDARD))
+
+    use data_encoding::BASE64;
+
+    //    let secret_key = "b0xxxxxx-c6xxxxxx-94xxxxxx-dxxxx";
+    //    let payload = "GET\napi.huobi.pro\n/v1/order/orders\nAccessKeyId=e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890";
+
     let signed_key = hmac::SigningKey::new(&digest::SHA256, secret.as_bytes());
-    let signature = hex_encode(hmac::sign(&signed_key, params.as_bytes()));
-    let base64_url = base64::encode(&signature.as_bytes());
-    base64_url
+    let signature = hmac::sign(&signed_key, params.as_bytes());
+    let b64_encoded_sig = BASE64.encode(signature.as_ref());
+
+    b64_encoded_sig
 }
