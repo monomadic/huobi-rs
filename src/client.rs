1b90bae89e047b199427365de6ed552ccfea1735
Rob Saunders <rob@wearebrandnew.com>
2019-04-15 22:20:26 +0800

    auth signing now working correctly for signed requests

--- src/client.rs
+++ src/client.rs
@@ -23,7 +23,7 @@ pub struct APIKey {
     secret_key: String,
 }
 
-static API_HOST: &'static str = "https://api.huobi.pro";
+static API_HOST: &'static str = "api.huobi.pro";
 
 impl Client {
     pub fn new(api_key: &str, secret_key: &str) -> Self {
@@ -43,34 +43,26 @@ impl Client {
         Ok(result)
     }
 
-    pub fn get_signed(&self, endpoint: &str, request: &str) -> APIResult<String> {
-        let url = format!("{}{}{}", API_HOST, endpoint, request);
-        let client = reqwest::ClientBuilder::new().build()?;
-
+    pub fn get_signed(&self, endpoint: &str, params: &str) -> APIResult<String> {
         let mut params: BTreeMap<String, String> = BTreeMap::new();
         params.insert("AccessKeyId".to_string(), self.api_key.clone());
         params.insert("SignatureMethod".to_string(), "HmacSHA256".to_string());
         params.insert("SignatureVersion".to_string(), "2".to_string());
+        params.insert("Timestamp".to_string(), get_timestamp());
 
-        // time
-        //        use percent_encoding::{utf8_percent_encode, PATH_SEGMENT_ENCODE_SET};
-        use std::time::{SystemTime, UNIX_EPOCH};
-
-        let utc_time = chrono::Utc::now();
-        let formatted_time = utc_time.format("%Y-%m-%dT%H%%3A%M%%3A%S").to_string();
-        println!("timestamp: {}", formatted_time.clone());
-        params.insert("Timestamp".to_string(), formatted_time);
-        // end time
-
-        let query_string = build_query_string(params);
-        let signature = sign_hmac_sha256_base64(&self.secret_key, &query_string).to_string();
-
-        use percent_encoding::{utf8_percent_encode, USERINFO_ENCODE_SET};
-        let signature = utf8_percent_encode(&signature, USERINFO_ENCODE_SET).to_string();
+        let params = build_query_string(params);
+        let signature = sign_hmac_sha256_base64(
+            &self.secret_key,
+            &format!("{}\n{}\n{}\n{}", "GET", API_HOST, endpoint, params,),
+        )
+        .to_string();
 
         let request = format!(
-            "{}{}?{}&Signature={}",
-            API_HOST, endpoint, query_string, signature
+            "https://{}{}?{}&Signature={}",
+            API_HOST,
+            endpoint,
+            params,
+            percent_encode(&signature.clone())
         );
 
         println!("request = {}", request.clone());
@@ -96,83 +88,6 @@ impl Client {
         let body = response.text()?;
         Ok(body)
     }
-
-    //    fn handler(&self, mut response: Response) -> APIResult<String> {}
-
-    //    if result. == "error".to_string() {
-    //    if let Some(err_msg) = result.err_code {
-    //    return Err(Box::new(HuobiError::ApiError(err_msg)));
-    //    } else {
-    //    return Err(Box::new(HuobiError::ApiError(format!(
-    //    "result dump: {:?}",
-    //    result
-    //    ))));
-    //    }
-    //    }
-    //    fn handler(&self, mut response: Response) -> APIResult<String> {
-    //        if response.status().is_success() {
-    //            Ok(response.text().unwrap())
-    //        } else {
-    //            Err(Box::new(response.error_for_status()))
-    //        }
-    //
-    //        StatusCode
-    //
-    //
-    ////        match response.status().as_u16() {
-    ////            StatusCode::from_u16(200) => {
-    ////                let mut body = String::new();
-    ////                response.read_to_string(&mut body).unwrap();
-    ////                Ok(body)
-    ////            }
-    ////            StatusCode::InternalServerError => {
-    ////                bail!("Internal Server Error");
-    ////            }
-    ////            StatusCode::ServiceUnavailable => {
-    ////                bail!("Service Unavailable");
-    ////            }
-    ////            StatusCode::Unauthorized => {
-    ////                bail!("Unauthorized");
-    ////            }
-    ////            StatusCode::BadRequest => {
-    ////                bail!(format!("Bad Request: {:?}", response));
-    ////            }
-    ////            s => {
-    ////                bail!(format!("Received response: {:?}", s));
-    ////            }
-    ////        }
-    //    }
-    //
-    //    fn nonce() -> String {
-    //        use std::time::{SystemTime, UNIX_EPOCH};
-    //        let start = SystemTime::now();
-    //        let since_the_epoch = start
-    //            .duration_since(UNIX_EPOCH)
-    //            .expect("Time went backwards");
-    //        let nonce = since_the_epoch.as_secs() * 1000; // +
-    //
-    //        nonce.to_string()
-    //    }
-
-    //    fn calculate_signature(&self, endpoint: &str, query_string: &str) -> String {}
-
-    //    fn build_headers(&self, signature: &str) -> APIResult<HeaderMap> {
-    //        let mut custom_headers = HeaderMap::new();
-    //
-    //        //        custom_headers.insert("KC-API-KEY", HeaderValue::from_str(&self.api_key)?);
-    //
-    //        //                              HeaderValue::new() self.api_key.as_str());
-    //        //        custom_headers.set_raw("KC-API-KEY", self.api_key.as_str());
-    //        //        custom_headers.set_raw("KC-API-NONCE", self::Client::nonce());
-    //        //        custom_headers.set_raw("KC-API-SIGNATURE", signature);
-    //        //
-    //        //        custom_headers.set_raw("HTTP_ACCEPT_LANGUAGE", "en-US");
-    //        //        custom_headers.set_raw("Accept-Language", "en-US");
-    //        //        custom_headers.set_raw("User-Agent", "kucoin-rs");
-    //        //        custom_headers.set_raw("Accept", "application/json");
-    //
-    //        Ok(custom_headers)
-    //    }
 }
 
 /// Compiles query string parameters into a http query string, in byte-order.
@@ -183,21 +98,14 @@ impl Client {
 /// params.insert("order-id".to_string(), "1234567890".to_string());
 /// params.insert("SignatureMethod".to_string(), "HmacSHA256".to_string());
 /// params.insert("SignatureVersion".to_string(), "2".to_string());
-/// params.insert("Timestamp".to_string(), "2017-05-11T15%3A19%3A30".to_string());
+/// params.insert("Timestamp".to_string(), "2017-05-11T15:19:30".to_string());
 ///
-/// assert_eq!(huobi::client::build_query_string(params), "AccessKeyId=e2xxxxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890".to_string());
+/// assert_eq!(huobi::client::build_query_string(params), "AccessKeyId=e2xxxxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890");
 /// ```
-pub fn build_query_string(mut parameters: BTreeMap<String, String>) -> String {
-    //    parameters.insert(
-    //        "AccessKeyId".to_string(),
-    //        "e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx".to_string(),
-    //    );
-    //    parameters.insert("SignatureVersion".to_string(), "2".to_string());
-    //    parameters.insert("SignatureMethod".to_string(), "HmacSHA256".to_string());
-
+pub fn build_query_string(parameters: BTreeMap<String, String>) -> String {
     parameters
         .into_iter()
-        .map(|(key, value)| format!("{}={}", key, value))
+        .map(|(key, value)| format!("{}={}", key, percent_encode(&value.clone())))
         .collect::<Vec<String>>()
         .join("&")
 }
@@ -205,17 +113,43 @@ pub fn build_query_string(mut parameters: BTreeMap<String, String>) -> String {
 /// Compiles query string parameters into a http query string, in byte-order.
 ///
 /// ```rust
-/// assert_eq!(huobi::client::sign_hmac_sha256_base64("b0xxxxxx-c6xxxxxx-94xxxxxx-dxxxx", "GET\napi.huobi.pro\n/v1/order/orders\nAccessKeyId=e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890"), "Nmd8AU8uAe0mkFpxNbiava0aeZzBEtYjCdie1ZYZjoM=");
+/// assert_eq!(huobi::client::sign_hmac_sha256_base64("b0xxxxxx-c6xxxxxx-94xxxxxx-dxxxx",
+///     "GET\napi.huobi.pro\n/v1/order/orders\nAccessKeyId=e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890"),
+///     "Nmd8AU8uAe0mkFpxNbiava0aeZzBEtYjCdie1ZYZjoM=");
 /// ```
-pub fn sign_hmac_sha256_base64(secret: &str, params: &str) -> String {
+pub fn sign_hmac_sha256_base64(secret: &str, digest: &str) -> String {
     use data_encoding::BASE64;
+
+    println!("digest: --{}--", digest);
 
     //    let secret_key = "b0xxxxxx-c6xxxxxx-94xxxxxx-dxxxx";
     //    let payload = "GET\napi.huobi.pro\n/v1/order/orders\nAccessKeyId=e2xxxxxx-99xxxxxx-84xxxxxx-7xxxx&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2017-05-11T15%3A19%3A30&order-id=1234567890";
 
     let signed_key = hmac::SigningKey::new(&digest::SHA256, secret.as_bytes());
-    let signature = hmac::sign(&signed_key, params.as_bytes());
+    let signature = hmac::sign(&signed_key, digest.as_bytes());
     let b64_encoded_sig = BASE64.encode(signature.as_ref());
 
     b64_encoded_sig
+}
+
+/// Percent encode parameter values for url distribution.
+///
+/// ```rust
+/// assert_eq!(huobi::client::percent_encode("2017-05-11T15:19:30"), "2017-05-11T15%3A19%3A30");
+/// ```
+pub fn percent_encode(source: &str) -> String {
+    use percent_encoding::{utf8_percent_encode, USERINFO_ENCODE_SET};
+    let signature = utf8_percent_encode(&source, USERINFO_ENCODE_SET).to_string();
+
+    signature
+}
+
+pub fn get_timestamp() -> String {
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    let utc_time = chrono::Utc::now();
+    let formatted_time = utc_time.format("%Y-%m-%dT%H:%M:%S").to_string();
+    println!("timestamp: {}", formatted_time.clone());
+
+    formatted_time
 }
